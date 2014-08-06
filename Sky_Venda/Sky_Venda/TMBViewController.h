3d85efa6065982b5de7e12dd246467f40022585b
Thiago Bernardes <thiagobernardes10@gmail.com>
2014-08-06 14:20:15 -0300

    Projeto para IOS criado

---
+++ Sky_Venda/Sky_Venda/TMBViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMBViewController.h
+//  Sky_Venda
+//
+//  Created by Thiago-Bernardes on 8/5/14.
+//  Copyright (c) 2014 TMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMBViewController : UIViewController
+
+@end
---
+++ Sky_Venda/Sky_Venda/TMBViewController.m
@@ -0,0 +1,29 @@
+//
+//  TMBViewController.m
+//  Sky_Venda
+//
+//  Created by Thiago-Bernardes on 8/5/14.
+//  Copyright (c) 2014 TMB. All rights reserved.
+//
+
+#import "TMBViewController.h"
+
+@interface TMBViewController ()
+
+@end
+
+@implementation TMBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
