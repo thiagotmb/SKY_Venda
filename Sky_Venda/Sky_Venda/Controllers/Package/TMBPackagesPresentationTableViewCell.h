42b73c7d02192019a07f80cb28dab620dd9e5294
Thiago Bernardes <thiagobernardes10@gmail.com>
2014-08-07 15:33:56 -0300

    Create Package Presentation Classes

---
+++ Sky_Venda/Sky_Venda/Controllers/Package/TMBPackagesPresentationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TMBPackagesPresentationTableViewCell.h
+//  Sky_Venda
+//
+//  Created by Thiago-Bernardes on 8/7/14.
+//  Copyright (c) 2014 TMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMBPackagesPresentationTableViewCell : UITableViewCell
+
+@end
---
+++ Sky_Venda/Sky_Venda/Controllers/Package/TMBPackagesPresentationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMBPackagesPresentationTableViewController.h
+//  Sky_Venda
+//
+//  Created by Thiago-Bernardes on 8/7/14.
+//  Copyright (c) 2014 TMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TMBPackagesPresentationTableViewCell.h"
+@interface TMBPackagesPresentationTableViewController : UITableViewController
+
+@end
